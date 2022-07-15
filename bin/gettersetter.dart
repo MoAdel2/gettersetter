3010a666d154b692ffcad4fa4b57463498a2b566
MoAdel2 <eng.adel1100@gmail.com>
2022-07-15 10:30:33 +0200

    modified proj

--- bin/gettersetter.dart
+++ bin/gettersetter.dart
@@ -1,5 +1,12 @@
-import 'package:gettersetter/gettersetter.dart' as gettersetter;
-
-void main(List<String> arguments) {
-  print('Hello world: ${gettersetter.calculate()}!');
+import 'package:gettersetter/gettersetter.dart';
+void main() { 
+   Employee emp = new Employee(); 
+   emp.emp_name = 'John'; 
+   emp.emp_age = 25; 
+   emp.emp_salary = 25000; 
+   print("W3Adda - Dart Getters and Setters Example.");
+   print("Employee's Name Is : ${emp.emp_name}"); 
+   print("Employee's Age Is : ${emp.emp_age}"); 
+   print("Employee's Salary Is : ${emp.emp_salary}"); 
 }
+
--- lib/gettersetter.dart
+++ lib/gettersetter.dart
@@ -1,3 +1,38 @@
-int calculate() {
-  return 6 * 7;
-}
+class Employee { 
+   String? empName; 
+   int? empAge; 
+   int? empSalary;
+    
+   String get emp_name { 
+      return empName!; 
+   } 
+    
+   void set emp_name(String name) { 
+      this.empName = name; 
+   } 
+   
+   void set emp_age(int age) { 
+      if(age<= 18) { 
+        print("Employee Age should be greater than 18 Years."); 
+      }  else { 
+         this.empAge = age; 
+      } 
+   } 
+   
+   int get emp_age { 
+      return empAge!;     
+   } 
+  
+  void set emp_salary(int salary) { 
+      if(salary<= 0) { 
+        print("Salary should be greater than 0"); 
+      }  else { 
+         this.empSalary = salary; 
+      } 
+   } 
+   
+   int get emp_salary { 
+      return empSalary!;     
+   } 
+  
+}  
